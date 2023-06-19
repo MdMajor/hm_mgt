c974eada7f17d3f0985a763cf4278db10ceefcc4
“i328377” <md.afsar.hussain@sap.com>
2023-06-19 16:16:47 +0530

    Init

---
+++ src/main/java/com/crashtech/hm_mgt/HmMgtApplication.java
@@ -0,0 +1,13 @@
+package com.crashtech.hm_mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmMgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crashtech/hm_mgt/HmMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crashtech.hm_mgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
